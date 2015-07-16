29625bdb33efd6cabe6a564d12856ee58d87f646
Yannick Baron <ill.baron.tokyo@gmail.com>
2015-07-16 22:23:21 +0900

    design design design

--- src/js/app.js
+++ src/js/app.js
@@ -143,4 +143,13 @@ directive('monsterGraph', function() {
 
     }  // link function end
   };
+}).
+
+directive('preventDefault', function() {
+    return function(scope, element, attrs) {
+        angular.element(element).bind('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    };
 });
