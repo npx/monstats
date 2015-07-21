e4b2a1957dcbccdcc38c77f4ad853f1d2ff17194
Yannick Baron <ill.baron.tokyo@gmail.com>
2015-07-21 18:21:51 +0900

    moved computation to service

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,3 @@
-// TODO refactor into service
-var whiteStats = ['atk', 'hp', 'rcv'];
-
-function growth(Lv, min, max, maxLv, g) {
-  return Math.round(min + (max - min) * Math.pow((Lv - 1) / (maxLv - 1), g));
-}
-
-function monster2graph(monster, stat) {
-  if (whiteStats.indexOf(stat) < 0) return [];
-
-  var stats = [];
-
-  var maxLv = monster.max_level;
-  var min = monster[stat + "_min"];
-  var max = monster[stat + "_max"];
-  var scale = monster[stat + "_scale"];
-
-  for (var i = 1; i < (maxLv + 1); i++) {
-    stats.push(growth(i, min, max, maxLv, scale));
-  }
-
-  return stats;
-}
-
 angular.module('App', []).
 
 controller('Main', function($scope, $http) {
@@ -63,7 +39,7 @@ controller('Main', function($scope, $http) {
 
 
 // TODO keep track of series and dynamically add and remove them
-directive('monsterGraph', function() {
+directive('monsterGraph', function(Growth) {
   return {
     restrict: 'E',
     scope: { data: '=monsters', stat: '@' },
@@ -71,10 +47,9 @@ directive('monsterGraph', function() {
     replace: true,
     link: function(scope, element, attrs) {
       // set type of stat
-      var stat = whiteStats[0];
-      if (('stat' in attrs) && (whiteStats.indexOf(attrs.stat) > -1))
+      var stat = Growth.types[0];
+      if (('stat' in attrs) && (Growth.isValidType(attrs.stat)))
         stat = attrs.stat;
-
 
       // configure the chart
       var canvas = element[0];
@@ -129,7 +104,7 @@ directive('monsterGraph', function() {
         angular.forEach(monsters, function(monster){
           var s = {};
           s.name = monster.name;
-          s.data = monster2graph(monster, stat);
+          s.data = Growth.computeGraph(monster, stat);
           chart.addSeries(s, false);
         });
         chart.redraw();
@@ -148,13 +123,59 @@ directive('monsterGraph', function() {
   };
 }).
 
+
+/**
+ * Growth computation Service
+ */
+service('Growth', function() {
+  // the growth function
+  var growthFunction = function (Lv, min, max, maxLv, g) {
+    return Math.round(min + (max - min) * Math.pow((Lv - 1) / (maxLv - 1), g));
+  };
+
+  // types of growth
+  this.types = ['atk', 'hp', 'rcv'];
+
+  this.isValidType = function(stype) {
+    return this.types.indexOf(stype) >= 0;
+  };
+
+  // compute the graph for the given monster and selected stat
+  this.computeGraph = function(monster, stype) {
+    // sanitize
+    if (!this.isValidType(stype)) {
+      console.log("Error: illegal stat type", stype);
+      return [];
+    }
+
+    // monster info
+    var maxLv = monster.max_level;
+    var min = monster[stype + "_min"];
+    var max = monster[stype + "_max"];
+    var scale = monster[stype + "_scale"];
+
+    // compute stat for all levels the monster can have
+    var stats = [];
+    for (var i = 1; i < (maxLv + 1); i++) {
+      stats.push(growthFunction(i, min, max, maxLv, scale));
+    }
+
+    // return the serie
+    return stats;
+  };
+}).
+
+
+/**
+ * Utilities
+ */
 directive('preventDefault', function() {
-    return function(scope, element, attrs) {
-        angular.element(element).bind('click', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-        });
-    };
+  return function(scope, element, attrs) {
+    angular.element(element).bind('click', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+    });
+  };
 }).
 
 filter('byName', function() {
