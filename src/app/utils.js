79103875ea2b2b5bf407a6c356037935d89578d1
Yannick Baron <ill.baron.tokyo@gmail.com>
2015-08-03 17:40:39 +0900

    ported utils

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,25 +1,34 @@
 /**
  * Monstats - Utilities
  */
-angular.module('monstats.utils', []).
+(function() {
+    'use strict';
 
+    angular
+        .module('monstats.utils', [])
+        .directive('preventDefault', preventDefault)
+        .filter('byName', byName);
 
-directive('preventDefault', [function() {
-    return function(scope, element, attrs) {
-        angular.element(element).bind('click', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-        });
-    };
-}]).
+    function preventDefault() {
+        return linkFunc;
 
+        function linkFunc(scope, el, attr) {
+            angular.element(el).bind('click', function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+            });
+        }
+    }
 
-filter('byName', [function() {
-    return function(monsters, q) {
-        if ((!(monsters)) || (!(q)) || (q.length < 3))
-            return [];
-        return monsters.filter(function(m) {
-            return m.name.toLowerCase().indexOf(q.toLowerCase()) > -1;
-        });
-    };
-}]);
+    function byName() {
+        return filter;
+
+        function filter(monsters, q) {
+            if ((!(monsters)) || (!(q)) || (q.length < 3))
+                return [];
+            return monsters.filter(function(m) {
+                return m.name.toLowerCase().indexOf(q.toLowerCase()) > -1;
+            });
+        }
+    }
+})();
